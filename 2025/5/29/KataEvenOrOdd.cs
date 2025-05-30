db7e973683645734dc0166be22a88e9376ab79ab
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-05-29 23:45:20 -0700

    Add project files.

---
+++ 2025/5/29/KataEvenOrOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars._2025._5._29
+{
+    internal class KataEvenOrOdd
+    {
+        public static string EvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+                return "Even";
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+                return "Odd";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using Codewars._2025._5._29;
+
+// Call static class methods:
+
+
+KataEvenOrOdd.EvenOrOdd(5);
