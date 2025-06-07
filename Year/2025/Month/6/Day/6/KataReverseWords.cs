007f0d88666e611df1c4955fba6bec851906fe5e
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-06-06 19:12:11 -0700

    Log Update

---
+++ Year/2025/Month/6/Day/6/KataReverseWords.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+internal class KataReverseWords
+{
+    /*
+    * Problem Description:
+    * Reverse characters in string. 
+    * Keep spaces in String at the same location.
+    */
+
+    public static string ReverseWords(string str)
+    {
+        var result = new StringBuilder();
+        var word = new StringBuilder();
+
+        foreach (char c in str)
+        {
+            if (c != ' ')
+            {
+                word.Append(c);
+            }
+
+            else
+            {
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    result.Append(word[i]);
+                }
+                result.Append(' ');
+                word.Clear();
+            }
+        }
+
+        for (int i = word.Length - 1; i >= 0; i--)
+        {
+            result.Append(word[i]);
+        }
+
+        return result.ToString();
+    }
+}
