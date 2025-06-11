8cc545cb78593208b3f70496af8a523cf0615e60
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-06-10 18:59:03 -0700

    Update Log

---
+++ Year/2025/Month/6/Day/10/KataRemoveVowel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class KataRemoveVowel
+{
+    /*
+    * Problem Description:
+    * Remove all vowels from a given string. 
+    */
+
+    public static string Disemvowel(string str)
+    {
+        var result = new StringBuilder();
+        foreach (char c in str)
+        {
+            char lower = char.ToLower(c);
+            if (lower != 'a' && lower != 'e' && lower != 'i' && lower != 'o' && lower != 'u')
+            {
+                result.Append(c);
+            }
+        }
+        return result.ToString();
+    }
+}
