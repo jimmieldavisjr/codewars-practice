7e598096e285a49a4ec8927346764b7942ca7bcf
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-06-13 20:59:51 -0700

    Log Update

---
+++ Year/2025/Month/6/Day/13/CountByX.cs
@@ -0,0 +1,28 @@
+﻿
+internal class CountByX
+{
+    /*
+    * Problem Description:
+    * Create a function with two arguments that will return an array of the first n multiples of x.
+    * x = 1, n = 10 --> [1,2,3,4,5,6,7,8,9,10]
+    * x = 2, n = 5  --> [2,4,6,8,10]
+    */
+
+    public static int[] CountBy(int x, int n)
+    {
+        int[] z = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+            {
+                z[i] = x;
+            }
+            else if (i > 0)
+            {
+                z[i] = z[i - 1] + x;
+            }
+        }
+        return z;
+    }
+}
