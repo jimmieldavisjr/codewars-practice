5041e002c3ee8c7cf869a83d786f3a0648a25638
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-06-17 18:40:29 -0700

    Log Update

---
+++ Year/2025/Month/6/Day/17/InvertValues.cs
@@ -0,0 +1,35 @@
+﻿
+internal class InvertValues
+{
+    /*
+    * Problem Description:
+    * Given a set of numbers, return the additive inverse of each
+    * [1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
+    */
+
+    public static class ArraysInversion
+    {
+        public static int[] InvertValues(int[] input)
+        {
+
+            if (input.Length == 0)
+            {
+                return new int[0];
+            }
+
+            else
+            {
+
+                int[] result = new int[input.Length];
+                int count = 0;
+
+                foreach (int num in input)
+                {
+                    result[count] = 0 - num;
+                    count++;
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ Year/2025/Month/6/Day/17/SimpleMultiplication.cs
@@ -0,0 +1,21 @@
+﻿
+
+internal class SimpleMultiplication
+{
+    /*
+    * Problem Description:
+    * This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+    */
+
+    public static int Multiply(int x)
+    {
+        if (x % 2 == 0)
+        {
+            return x * 8;
+        }
+        else
+        {
+            return x * 9;
+        }
+    }
+}
