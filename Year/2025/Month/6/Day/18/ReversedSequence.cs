c05a00f464431410e7d54ea6fcf4593ea26451c6
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-06-19 08:07:15 -0700

    Log Update

---
+++ Year/2025/Month/6/Day/18/ReversedSequence.cs
@@ -0,0 +1,20 @@
+﻿
+internal class ReversedSequence
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+
+    public static int[] ReverseSeq(int n)
+    {
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = n - i;
+        }
+        return arr;
+    }
+}
