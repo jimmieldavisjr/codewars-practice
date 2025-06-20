fe5a6dfc44c4dcd0c316bf751273917b29d713cf
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-06-20 09:46:13 -0700

    Log Update

---
+++ Year/2025/Month/6/Day/20/ReturningStrings.cs
@@ -0,0 +1,13 @@
+﻿
+internal class ReturningStrings
+{
+    /*
+     * Problem Description:
+     * Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?"
+     */
+
+    public static string Greet(string name)
+    {
+        return new string($"Hello, {name} how are you doing today?");
+    }
+}
---
+++ Year/2025/Month/6/Day/20/SumOfNumbers.cs
@@ -0,0 +1,41 @@
+﻿
+internal class SumOfNumbers
+{
+    /*
+     * Problem Description:
+     * Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+     */
+
+    public class Sum
+    {
+        public int GetSum(int a, int b)
+        {
+            int result = 0;
+
+            if (a == b)
+            {
+                return a;
+            }
+
+            else if (a < b)
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    result += i;
+                }
+                return result;
+            }
+
+            else if (b < a)
+            {
+                for (int i = b; i <= a; i++)
+                {
+                    result += i;
+                }
+                return result;
+            }
+
+            return result;
+        }
+    }
+}
