d3dd8e1531c60c8c1f5f6010a3e0d60a0a49d669
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-06-21 22:26:55 -0700

    Log Update

---
+++ Year/2025/Month/6/Day/21/SumArrays.cs
@@ -0,0 +1,23 @@
+﻿
+internal class SumArrays
+{
+    /*
+    * Problem Description:
+    * Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+    */
+
+    public static double SumArray(double[] array)
+    {
+        if (array.Length == 0)
+        {
+            return 0;
+        }
+        double result = 0;
+
+        foreach (double num in array)
+        {
+            result += num;
+        }
+        return result;
+    }
+}
