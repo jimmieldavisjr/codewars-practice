28893a73d902619a03dae5696d05bb3cc0e9ec28
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-06-22 22:25:12 -0700

    Log Update

---
+++ Year/2025/Month/6/Day/22/SquareNum.cs
@@ -0,0 +1,26 @@
+﻿
+internal class SquareNum
+{
+    /*
+    * Problem Description:
+    * Given an integral number, determine if it's a square number:
+    */
+
+    public static bool IsSquare(int n)
+    {
+        if (n < 0)
+        {
+            return false;
+        }
+
+        if (Math.Sqrt(n) == Math.Floor(Math.Sqrt(n)))
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+}
