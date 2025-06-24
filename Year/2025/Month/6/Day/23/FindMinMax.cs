52db79d6b916bbf589e3767554d7e11b46752c3e
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-06-23 18:32:24 -0700

    Log Update

---
+++ Year/2025/Month/6/Day/23/FindMinMax.cs
@@ -0,0 +1,34 @@
+﻿
+internal class FindMinMax
+{
+    /*
+    * Problem Description:
+    * Write a function that returns both the minimum and maximum number of the given list/array.
+    */
+
+    public static int[] minMax(int[] lst)
+    {
+        int low = 0;
+        int high = 0;
+
+        for (int i = 0; i < lst.Length; i++)
+        {
+            if (i == 0)
+            {
+                low = lst[i];
+                high = lst[i];
+            }
+
+            else if (lst[i] >= high)
+            {
+                high = lst[i];
+            }
+
+            else if (lst[i] <= low)
+            {
+                low = lst[i];
+            }
+        }
+        return new int[2] { low, high };
+    }
+}
