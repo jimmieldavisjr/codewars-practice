441f2fd0df7055d9b507364a2c9c2766e97f6f3e
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-04 14:34:21 -0700

    Log Update

---
+++ Year/2025/Month/7/Day/04/ReverseStrings.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+internal class ReverseStrings
+{
+    /*
+    * Problem Description:
+    * Complete the solution so that it reverses the string passed into it. 'world'  =>  'dlrow'
+    */
+
+    public static string Solution(string str)
+    {
+        var sb = new StringBuilder();
+
+        for (int i = str.Length; i > 0; i--)
+        {
+            sb.Append(str[i - 1]);
+        }
+        return sb.ToString();
+    }
+}
