a94e3f2bcf18ff632c8d018f2204b988192353d2
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-08 21:31:51 -0700

    Updated WhoLikesIt.cs - improved method

--- Year/2025/Month/7/Day/08/WhoLikesIt.cs
+++ Year/2025/Month/7/Day/08/WhoLikesIt.cs
@@ -14,29 +14,13 @@ internal class WhoLikesIt
 
     public static string Likes(string[] name)
     {
-        if (name.Length == 1)
+        switch (name.Length)
         {
-            return $"{name[0]} likes this";
-        }
-
-        else if (name.Length == 2)
-        {
-            return $"{name[0]} and {name[1]} like this";
-        }
-
-        else if (name.Length == 3)
-        {
-            return $"{name[0]}, {name[1]} and {name[2]} like this";
-        }
-
-        else if (name.Length > 3)
-        {
-            return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
-        }
-
-        else
-        {
-            return "no one likes this";
+            case 0: return "no one likes this";
+            case 1: return $"{name[0]} likes this";
+            case 2: return $"{name[0]} and {name[1]} like this";
+            case 3: return $"{name[0]}, {name[1]} and {name[2]} like this";
+            default: return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
         }
     }
 }
