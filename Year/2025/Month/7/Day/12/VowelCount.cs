61e017b19d18867d29297687006e9144c0d8da0a
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-13 00:00:08 -0700

    Log Update

---
+++ Year/2025/Month/7/Day/12/VowelCount.cs
@@ -0,0 +1,50 @@
+﻿/// <summary>
+/// Provides functionality for counting total occurrences of vowels in a string.
+/// </summary>
+///
+/// <remarks>
+/// Codewars Challenge: Vowel Count — 7 kyu.  
+/// 
+/// Problem: Return the number (count) of vowels in the given string.
+/// </remarks>
+internal class VowelCount
+{
+    /// <summary>
+    /// Counts the total occurrences of vowels in the input string value     
+    /// </summary>
+    /// <param name="n">The input string containing possible vowels.</param>
+    /// <returns>
+    /// The total count of vowels as int.
+    /// </returns>
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+
+        foreach (char c in str)
+        {
+            switch (c)
+            {
+                case 'a':
+                    vowelCount++;
+                    break;
+
+                case 'e':
+                    vowelCount++;
+                    break;
+
+                case 'i':
+                    vowelCount++;
+                    break;
+
+                case 'o':
+                    vowelCount++;
+                    break;
+
+                case 'u':
+                    vowelCount++;
+                    break;
+            }
+        }
+        return vowelCount;
+    }
+}
