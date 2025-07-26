d1d0958aba6f9f3fca2ca81a1a2d417408e2ed1b
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-25 17:44:29 -0700

    feat: Solve Codewars: 'Are They The Same' - 6 Kyu

--- Year/2025/Month/7/Day/25/ArrayMultiplicities.cs
+++ Year/2025/Month/7/Day/25/ArrayMultiplicities.cs
@@ -1,15 +1,15 @@
-﻿
-// Unsolved. 1 Test case failing.
-// TBD.
-// https://www.codewars.com/kata/550498447451fbbd7600041c/train/csharp
-
-internal static class ArrayMultiplicities
+﻿internal static class ArrayMultiplicities
 {
     public static bool comp(int[] a, int[] b)
     {
         if (a == null || b == null || a.Length == 0 || b.Length == 0 || a.Length != b.Length)
         {
             return false;
+        }
+
+        if (a.Length == 0 && b.Length == 0)
+        {
+            return true;
         }
 
         var listA = a.ToList();
