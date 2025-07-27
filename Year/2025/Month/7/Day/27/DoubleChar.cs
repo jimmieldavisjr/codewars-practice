a4f43c870505cb0401964517b7cde60a265808e7
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-27 12:36:06 -0700

    feat: Solve Codewars: DoubleChar - 8 Kyu

---
+++ Year/2025/Month/7/Day/27/DoubleChar.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+internal class DoubleChar
+{
+    public class Kata
+    {
+        public static string DoubleChar(string s)
+        {
+            var result = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                result.Append(c);
+                result.Append(c);
+            }
+
+            return result.ToString();
+        }
+    }
+}
