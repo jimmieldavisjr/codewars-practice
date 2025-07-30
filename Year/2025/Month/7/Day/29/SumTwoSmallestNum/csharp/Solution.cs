71f8364cf4bef6c7c5829e175efd065f1980bbd1
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-29 21:50:23 -0700

    Solve: Codewars - Sum of Two Lowest Positive Integers - 7 Kyu

---
+++ Year/2025/Month/7/Day/29/SumTwoSmallestNum/csharp/Solution.cs
@@ -0,0 +1,27 @@
+﻿internal class Solution
+{
+    public static int sumTwoSmallestNumbers(int[] numbers)
+    {
+        int[] arr = { numbers[0] < numbers[1] ? numbers[0] : numbers[1], numbers[0] < numbers[1] ? numbers[1] : numbers[0] };
+        int count = 0;
+
+        foreach (int num in numbers)
+        {
+            if (count > 1)
+            {
+                if (num < arr[0])
+                {
+                    arr[1] = arr[0];
+                    arr[0] = num;
+                }
+
+                else if (num < arr[1])
+                {
+                    arr[1] = num;
+                }
+            }
+            count++;
+        }
+        return arr[0] + arr[1];
+    }
+}
