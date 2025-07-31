1597ca24756e97193ba2f4fb5becf3e514eb345b
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-30 22:14:13 -0700

    Solve: Codewars - Switch It Up - 8 Kyu

---
+++ Year/2025/Month/7/Day/30/SwitchItUp/csharp/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars.Year._2025.Month._7.Day._30.SwitchItUp.csharp;
+
+internal class Solution
+{
+    public static string SwitchItUp(int number)
+    {
+        switch (number)
+        {
+            case 0:
+                return "Zero";
+
+            case 1:
+                return "One";
+
+            case 2:
+                return "Two";
+
+            case 3:
+                return "Three";
+
+            case 4:
+                return "Four";
+
+            case 5:
+                return "Five";
+
+            case 6:
+                return "Six";
+
+            case 7:
+                return "Seven";
+
+            case 8:
+                return "Eight";
+
+            case 9:
+                return "Nine";
+
+            default:
+                return "Error";
+        }
+    }
+}
