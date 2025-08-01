0a8014db76bf08a67e6166a0fa34299de3067e43
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-31 18:37:49 -0700

    Solve: Codewars - Find Unique Int - 6 Kyu

--- Year/2025/Month/7/Day/31/FindUniqueInt/csharp/Solution.cs
+++ Year/2025/Month/7/Day/31/FindUniqueInt/csharp/Solution.cs
@@ -24,3 +24,39 @@ namespace Codewars.Year._2025.Month._7.Day._31.FindUniqueInt.csharp
         }
     }
 }
+
+/*
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+  public static int GetUnique(IEnumerable<int> numbers)
+  {
+    int[] arr = numbers.ToArray();
+    int u = arr[0];
+    
+    for (int i = 1; i < arr.Length; i++)
+    {
+      
+      if (i == arr.Length - 1)
+      {
+        u = arr[i];
+        return u;
+      }
+      
+      if (u != arr[i] && u != arr[i+1])
+      {
+        return u;
+      }
+      
+      if (arr[i] != u && arr[i] != arr[i+1])
+      {
+        u = arr[i];
+        return u;
+      }
+    }
+    return u;
+  }
+}
+ */
