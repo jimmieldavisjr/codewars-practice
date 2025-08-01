536c6c530011a03276748b8770d651b6b5300282
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-31 18:47:28 -0700

    Solve: Codewars - Total Amount of Points - 8 Kyu

---
+++ Year/2025/Month/7/Day/31/TotalPoints/csharp/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars.Year._2025.Month._7.Day._31.TotalPoints.csharp
+{
+    internal class Solution
+    {
+        public static int TotalPoints(string[] games)
+        {
+            int points = 0;
+
+            for (int i = 0; i < games.Length; i++)
+            {
+                if (games[i][0] > games[i][2])
+                {
+                    points += 3;
+                }
+
+                if (games[i][0] == games[i][2])
+                {
+                    points += 1;
+                }
+            }
+            return points;
+        }
+    }
+}
