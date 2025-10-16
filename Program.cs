c808fb28410e31e6e9fa0b5eaf0baf99a35ce392
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-10-15 20:22:02 -0700

    Solved Codewars - 8 Kyu - Debug Say Hello

--- Program.cs
+++ Program.cs
@@ -1,3 +1,1 @@
-﻿using Codewars.Year._2025.Month._8.Day._26.SquareEveryDigit.csharp;
-
-Kata.SquareDigits(9119);
+﻿
---
+++ Year/2025/Month/10/Day/15/DebugHello/csharp/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Codewars.Year._2025.Month._8.Day._25.TwoSum.csharp
+{
+    // Codewars: Debug Say Hello - 8 Kyu
+    // Problem: https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/csharp
+
+    public class Solution
+    {
+        public static string SayHello(string name)
+        {
+            return $"Hello, {name}";
+        }
+    }
+}
