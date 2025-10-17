eaba0358f14c6c655fabb401b62780e8d1b33e3d
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-10-17 13:52:31 -0700

    feat(codewars): add solution for BasicMathmaticalOperations & SayHelloDebug

--- Year/2025/Month/10/Day/15/DebugHello/csharp/Solution.cs
+++ Year/2025/Month/10/Day/15/DebugHello/csharp/Solution.cs
@@ -5,11 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace Codewars.Year._2025.Month._8.Day._25.TwoSum.csharp
-{
-    // Codewars: Debug Say Hello - 8 Kyu
-    // Problem: https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/csharp
+// Codewars: Debug Say Hello - 8 Kyu
+// Problem: https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/csharp
 
+namespace SayHelloDebugCsharp
+{
     public class Solution
     {
         public static string SayHello(string name)
---
+++ Year/2025/Month/10/Day/17/BasicMathematicalOperations/csharp/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+// Codewars: Basic Mathematical Operations - 8 Kyu
+// Problem: https://www.codewars.com/kata/57356c55867b9b7a60000bd7
+
+namespace BasicMathmaticalOperatorsCsharp
+{
+    public class Solution
+    {
+        public static double basicOp(char operation, double value1, double value2)
+        {
+            switch (operation)
+            {
+                case '+':
+                    return value1 + value2;
+                case '-':
+                    return value1 - value2;
+                case '/':
+                    return value1 / value2;
+                case '*':
+                    return value1 * value2;
+
+                default:
+                    return 0.0;
+            }
+        }
+    }
+}
