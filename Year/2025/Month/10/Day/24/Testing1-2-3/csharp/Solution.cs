780d121d9e5f10ea558914cd114e26c6bf4b39dd
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-10-24 06:00:09 -0700

    feat(csharp): completed codewars challenge 'testing 1-2-3'

---
+++ Year/2025/Month/10/Day/24/Testing1-2-3/csharp/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Codewars: Testing 1-2-3 - 7 Kyu
+// Problem: https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/csharp
+
+namespace Codewars.Year._2025.Month._10.Day._24.Testing1_2_3.csharp
+{
+    internal class Solution
+    {
+        public static List<string> Number(List<string> lines)
+        {
+            List<string> result = new List<string>();
+            int curr = 1;
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                result.Add($"{curr}: {lines[i]}");
+                curr++;
+            }
+
+            return result;
+        }
+    }
+}
