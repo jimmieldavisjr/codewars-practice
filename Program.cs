dcf23e6afd77595ad06be3d7d4d5dd457c9aa11f
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-11-12 18:53:18 -0800

    feat(solve): solved Codewars 5 Kyu Mean Square Error C#

--- Program.cs
+++ Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public class TopWords
+{
+    public static List<string> Top3(string s)
+    {
+        List<string> result = new List<string>();
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (char.IsLetter(s[i]))
+            {
+                sb.Append(char.ToLower(s[i]));
+                continue;
+            }
+
+            if (sb.Length > 0 && !char.IsLetter(s[i]))
+            {
+                if (!dict.ContainsKey(sb.ToString()))
+                {
+                    dict[sb.ToString()] = 1;
+                }
+
+                else
+                {
+                    dict[sb.ToString()] += 1;
+                }
+                sb.Length = 0;
+            }
+        }
+
+        return dict
+          .OrderByDescending(p => p.Value)
+          .Take(3)
+          .Select(p => p.Key)
+          .ToList();
+    }
+}
---
+++ Year/2025/Month/11/Day/12/MeanSquareError/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars.Year._2025.Month._11.Day._12.MeanSquareError
+{
+    internal class Solution
+    {
+        public static double MeanSquareError(int[] firstArray, int[] secondArray)
+        {
+            int len = firstArray.Length;
+            double res = 0;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                int temp = (secondArray[i] - firstArray[i]);
+                res += temp * temp;
+            }
+
+            res = res / len;
+
+            return res;
+        }
+    }
+}
