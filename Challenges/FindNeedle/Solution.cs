3f627456aa4a4584fa8d38b822efba4fe61a9e17
Jimmie Davis <life@jimmieldavisjr.com>
2026-01-16 15:29:01 -0800

    refactor/feat(solution/challenge): solution directory refactored/organized for simplicity. add new challenge completion

---
+++ Challenges/FindNeedle/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars.Challenges.FindNeedle
+{
+    internal class Solution
+    {
+        public static string FindNeedle(object[] haystack)
+        {
+            int i = 0;
+            foreach (object item in haystack)
+            {
+                if (item is string s && s == "needle")
+                {
+                    return $"found the needle at position {i}";
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return "No items found";
+        }
+    }
+}
